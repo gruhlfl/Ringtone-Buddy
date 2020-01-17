7f215e26871ccdc6cd274dc2f7d01e767c25f915
Glen <gruhl@frontlineed.com>
2020-01-17 10:19:05 -0500

    wip

--- Ringtone Buddy/ViewController.swift
+++ Ringtone Buddy/ViewController.swift
@@ -1,43 +1,5 @@
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController, UNUserNotificationCenterDelegate {
-    
-    private var audioPlayer: AVAudioPlayer?
+class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) {
-            (granted, error) in
-            if granted {
-                print("yes")
-            } else {
-                print("No")
-            }
-        }
-        
-        UNUserNotificationCenter.current().delegate = self
-        
-        // 1
-        let content = UNMutableNotificationContent()
-        content.title = "Notification Tutorial"
-        content.subtitle = "from ioscreator.com"
-        content.body = " Notification triggered"
-//        content.sound = UNNotificationSound.default
-        
-//        content.sound = UNNotificationSound(named:UNNotificationSoundName(rawValue: "notification.wav"))
-
-        // 3
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
-        let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
-        
-        // 4
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        print("willPresent??")
-    }
 }
